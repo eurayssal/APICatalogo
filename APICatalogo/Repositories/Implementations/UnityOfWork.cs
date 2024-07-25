f341f2c02d75d40f9c5058635de3c032562d5358
eurayssal <eurayssamagalhaes@gmail.com>
2024-07-25 10:13:31 -0300

    UnityOfWork adicionada

---
+++ APICatalogo/Repositories/Implementations/UnityOfWork.cs
@@ -0,0 +1,44 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Repositories.Interfaces;
+
+namespace APICatalogo.Repositories.Implementations
+{
+    public class UnityOfWork : IUnityOfWork
+    {
+        private IProdutoRepository? _produtoRepo;
+        private ICategoriaRepository? _categoriaRepo;
+
+        public AppDbContext _context;
+
+        public UnityOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IProdutoRepository ProdutoRepository
+        {
+            get
+            {
+                return _produtoRepo = _produtoRepo ?? new ProdutoRepository(_context);
+            }
+        }
+
+        public ICategoriaRepository CategoriaRepository
+        {
+            get
+            {
+                return _categoriaRepo ?? new CategoriaRepository(_context);
+            }
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ APICatalogo/Repositories/Interfaces/IUnityOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace APICatalogo.Repositories.Interfaces
+{
+    public interface IUnityOfWork
+    {
+        //Eu poderia implementar o repositório genérico mas perderia a capacidade de implementar métodos personalizados
+        //IRepository<Produto> ProdutoRepository { get; }
+        IProdutoRepository ProdutoRepository { get; }
+        ICategoriaRepository CategoriaRepository { get; }
+        void Commit();
+    }
+}
