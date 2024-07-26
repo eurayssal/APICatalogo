044e1324d5d2d091ba5e2a8b0c0945a074dcbc36
eurayssal <eurayssamagalhaes@gmail.com>
2024-07-26 09:13:06 -0300

    Usando metodos de extensão

---
+++ APICatalogo/DTOs/Mappings/CategoriaDTOMappingExtensions.cs
@@ -0,0 +1,44 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.DTOs.Mappings
+{
+    public static class CategoriaDTOMappingExtensions
+    {
+        public static CategoriaDTO? ToCategoriaDTO(this Categoria categoria)
+        {
+            if (categoria == null) return null;
+
+            return new CategoriaDTO
+            {
+                CategoriaId = categoria.CategoriaId,
+                Nome = categoria.Nome,
+                ImagemUrl = categoria.ImagemUrl,
+            };
+        }
+
+        public static Categoria? ToCategoria(this CategoriaDTO categoriaDTO)
+        {
+            if (categoriaDTO == null) return null;
+
+            return new Categoria
+            {
+                CategoriaId = categoriaDTO.CategoriaId,
+                Nome = categoriaDTO.Nome,
+                ImagemUrl = categoriaDTO.ImagemUrl,
+            };
+        }
+
+        public static IEnumerable<CategoriaDTO> ToCategoriaDTOList(this IEnumerable<Categoria> categorias)
+        {
+            //Se eu não tiver nenhuma das categorias eu retorno uma lista vazia [] ou new List<Categoria>();
+            if (categorias == null || !categorias.Any()) return [];
+
+            return categorias.Select(categoria => new CategoriaDTO
+            {
+                CategoriaId = categoria.CategoriaId,
+                Nome = categoria.Nome,
+                ImagemUrl = categoria.ImagemUrl,
+            }).ToList();
+        }
+    }
+}
