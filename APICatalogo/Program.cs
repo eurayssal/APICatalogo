8b336f94977b70b84a70bf1638473ee4ffb686ac
eurayssal <eurayssamagalhaes@gmail.com>
2024-12-05 11:22:14 -0300

    Implementação da geração do refresh token

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -2,13 +2,19 @@ using APICatalogo.Context;
 using APICatalogo.DTOs.Mappings;
 using APICatalogo.Filters;
 using APICatalogo.Logging;
+using APICatalogo.Models;
 using APICatalogo.Repositories.Implementations;
 using APICatalogo.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+var secretKey = builder.Configuration["JWT:SecretKey"]
+                ?? throw new ArgumentException("Invalid secret key!!");
 
 // Add services to the container.
 builder.Services.AddControllers()
@@ -20,17 +26,38 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<AppDbContext>()
                 .AddDefaultTokenProviders();
 
-builder.Services.AddAuthentication("Bearer").AddJwtBearer();
-
-string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+string mySqlConnection = builder.Configuration
+                                .GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
                     options.UseMySql(mySqlConnection,
                     ServerVersion.AutoDetect(mySqlConnection)));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false; //Em prd isso deve ser true
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero,
+        ValidAudience = builder.Configuration["JWT: ValidAudience"],
+        ValidIssuer = builder.Configuration["JWT: ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+                                Encoding.UTF8.GetBytes(secretKey))
+    };
+});
 
 builder.Services.AddScoped<ApiLoggingFilter>();
 
---
+++ APICatalogo/Services/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace APICatalogo.Services
+{
+    public interface ITokenService
+    {
+        //Vai receber uma lista de claims 
+        JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config);
+    }
+}
